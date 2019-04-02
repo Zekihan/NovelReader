330cf978e63bfc40d6bf922b01f0677a3e44b805
Zekihan <38537758+Zekihan@users.noreply.github.com>
2019-04-02 16:35:48 +0300

    as

--- app/src/main/java/com/zekihan/novelreader/activities/main/SupportFragment.java
+++ app/src/main/java/com/zekihan/novelreader/activities/main/SupportFragment.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,50 +64,59 @@ public class SupportFragment extends Fragment {
             }
         });
 
-        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(mContext);
-        mRewardedVideoAd.loadAd(getResources().getString(R.string.rewarded_video_ad_unit_id), new AdRequest.Builder().build());
-        mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
-            @Override
-            public void onRewardedVideoAdLoaded() {
-            }
+        try{
+            mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(mContext);
+            mRewardedVideoAd.loadAd(getResources().getString(R.string.rewarded_video_ad_unit_id), new AdRequest.Builder().build());
+            mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
+                @Override
+                public void onRewardedVideoAdLoaded() {
+                }
 
-            @Override
-            public void onRewardedVideoAdOpened() {
-            }
+                @Override
+                public void onRewardedVideoAdOpened() {
+                }
 
-            @Override
-            public void onRewardedVideoStarted() {
-            }
+                @Override
+                public void onRewardedVideoStarted() {
+                }
 
-            @Override
-            public void onRewardedVideoAdClosed() {
-            }
+                @Override
+                public void onRewardedVideoAdClosed() {
+                }
 
-            @Override
-            public void onRewarded(RewardItem rewardItem) {
-                Toast.makeText(mContext, "Appreciating the support", Toast.LENGTH_SHORT).show();
-            }
+                @Override
+                public void onRewarded(RewardItem rewardItem) {
+                    Toast.makeText(mContext, "Appreciating the support", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onRewardedVideoAdLeftApplication() {
-            }
+                @Override
+                public void onRewardedVideoAdLeftApplication() {
+                }
 
-            @Override
-            public void onRewardedVideoAdFailedToLoad(int i) {
-                mRewardedVideoAd.loadAd(getResources().getString(R.string.rewarded_video_ad_unit_id), new AdRequest.Builder().build());
-            }
+                @Override
+                public void onRewardedVideoAdFailedToLoad(int i) {
+                    try{
+                        mRewardedVideoAd.loadAd(getResources().getString(R.string.rewarded_video_ad_unit_id), new AdRequest.Builder().build());
+                    }catch (Exception e){
+                        Log.e(TAG,e.toString());
+                    }
+                }
 
-            @Override
-            public void onRewardedVideoCompleted() {
-            }
-        });
+                @Override
+                public void onRewardedVideoCompleted() {
+                }
+            });
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mRewardedVideoAd.show();
-            }
-        });
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mRewardedVideoAd.show();
+                }
+            });
+        }catch (Exception e){
+            Log.e(TAG,e.toString());
+        }
+
 
         return rootView;
     }
